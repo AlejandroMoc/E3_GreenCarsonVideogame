ccecde414575e6194131c698f7603ad40a93e9fc
Alejandro M. <a01736353@tec.mx>
2023-09-29 16:43:54 -0600

    Actualizada la segunda pantalla, de selección de nivel, para incluir elementos, strings y gráficos requeridos. Añadido soporte al idioma inglés por defecto (la aplicación cambiará al idioma del sistema).

--- GreenCarsonVideojuego/app/src/main/java/greencarson/videojuego/SelectLevelActivity.java
+++ GreenCarsonVideojuego/app/src/main/java/greencarson/videojuego/SelectLevelActivity.java
@@ -1,26 +1,27 @@
 package greencarson.videojuego;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
-public class RegisterActivity extends AppCompatActivity {
+public class SelectLevelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Ocultar barra de status
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_selectlevel);
     }
 
     //Para cambiar pantalla
-    public void goToNextScreen(View v){
-        Intent intent = new Intent(this, SelectLevelActivity.class);
-        startActivity(intent);
-    }
+//    public void goToNextScreen(View v){
+//        Intent intent = new Intent(this, SelectLevelActivity.class);
+//        startActivity(intent);
+//    }
 
     //Tras haber creadolo, lo unimos con el onClick, seleccionando la función correspondiente
 
