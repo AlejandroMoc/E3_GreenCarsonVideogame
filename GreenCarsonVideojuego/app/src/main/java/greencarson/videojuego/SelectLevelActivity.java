64ffe9b071c5096ce64280502d1f03ac0f46b9ed
Alejandro M. <a01736353@tec.mx>
2023-10-03 12:55:52 -0600

    Solucionado fallo en el que no funcionaba el botón de Iniciar en la primera pantalla.

--- GreenCarsonVideojuego/app/src/main/java/greencarson/videojuego/SelectLevelActivity.java
+++ GreenCarsonVideojuego/app/src/main/java/greencarson/videojuego/SelectLevelActivity.java
@@ -2,9 +2,8 @@ package greencarson.videojuego;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 
 public class SelectLevelActivity extends AppCompatActivity {
@@ -13,12 +12,12 @@ public class SelectLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Ocultar barra de status
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_selectlevel);
     }
 
     //Para cambiar pantalla
-//    public void goToNextScreen(View v){
+//    public void goToBackScreen(View v){
 //        Intent intent = new Intent(this, SelectLevelActivity.class);
 //        startActivity(intent);
 //    }
