725396f6981b12a596aeb283fcf3922c38348b6a
Alejandro M. <a01736353@tec.mx>
2023-10-05 10:36:17 -0600

    Añadida declaración de las variables que se utilizarán en ambas pantallas. Añadido sistema de selección de niveles en la pantalla de selección de niveles. Aún falta cambiar el aspecto visual de los botones que no estén seleccionados, así como ligar a pantalla de nivel.

--- GreenCarsonVideojuego/app/src/main/java/greencarson/videojuego/RegisterActivity.java
+++ GreenCarsonVideojuego/app/src/main/java/greencarson/videojuego/RegisterActivity.java
@@ -5,8 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //Declarar variables
+    EditText aTxt;
+    EditText bTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,13 +19,24 @@ public class RegisterActivity extends AppCompatActivity {
         //Ocultar barra de status
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_register);
+
+        //Ligar elementos a la pantalla
+        aTxt=findViewById(R.id.textEmail);
+        bTxt=findViewById(R.id.textPassword);
     }
 
-    //Para cambiar pantalla
+    //Para hacer login
     public void goToNextScreen(View v){
+
+        //Recuperar strings de login
+        String stringEmail = aTxt.getText().toString();
+        String stringPassword = bTxt.getText().toString();
+
+        //Falta hacer login antes de hacer intento
         Intent intent = new Intent(this, SelectLevelActivity.class);
         startActivity(intent);
     }
+
 
     //Tras haber creadolo, lo unimos con el onClick, seleccionando la función correspondiente
 
--- GreenCarsonVideojuego/app/src/main/java/greencarson/videojuego/SelectLevelActivity.java
+++ GreenCarsonVideojuego/app/src/main/java/greencarson/videojuego/SelectLevelActivity.java
@@ -3,10 +3,14 @@ package greencarson.videojuego;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 
 public class SelectLevelActivity extends AppCompatActivity {
+
+    //Declarar variables
+    int levelNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,41 @@ public class SelectLevelActivity extends AppCompatActivity {
         //Ocultar barra de status
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_selectlevel);
+    }
+
+    //Para seleccionar nivel
+    public void selectLevel(View v) {
+        switch(v.getId()) {
+            case R.id.buttonBasic:
+                levelNumber = 1;
+                break;
+            case R.id.buttonInter:
+                levelNumber = 2;
+                break;
+            case R.id.buttonAdv:
+                levelNumber = 3;
+                break;
+        }
+    }
+
+    //Para empezar nivel
+    public void startLevel(View v){
+
+        //Seleccionar nivel
+        switch (levelNumber) {
+            case 1:
+                Log.d("1", "Se envia a nivel 1");
+                break;
+            case 2:
+                Log.d("2", "Se envia a nivel 2");
+                break;
+            case 3:
+                Log.d("3", "Se envia a nivel 3");
+                break;
+            default:
+                Log.d("0", "Se envia a nivel 1");
+                break;
+        }
     }
 
     //Para regresar
