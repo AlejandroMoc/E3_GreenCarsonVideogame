658f4f0e8d36c59cdb9c32e09d7c484a6d867e14
Alejandro M. <a01736353@tec.mx>
2023-12-01 14:10:35 -0600

    Cambios en el LoginActivity.java

--- GreenCarsonVideojuego/app/src/main/java/greencarson/videojuego/LoginActivity.java
+++ GreenCarsonVideojuego/app/src/main/java/greencarson/videojuego/LoginActivity.java
@@ -29,7 +29,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
